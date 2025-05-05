eb3d544143ae71cdd1df2b600c3eedd40eef5f6b
Zheleznii <molokov.0505@mail.ru>
2025-05-05 18:28:23 +0300

    2.2.py

--- 2.2.py
+++ 2.2.py
@@ -2,8 +2,7 @@ dct = {1: 11, 2: 22, 3: 33, 4: 4, 5: 33, 6: 1}
 
 keys_set = set(dct.keys())
 values_set = set(dct.values())
-union_set = keys_set.union(values_set) # можно через keys_set | values_set но я не знаю как тебе нужно так что сам выбери !!!!!!!!!
-
+union_set = keys_set.union(values_set) 
 print("Множество ключей:", keys_set)
 print("Множество значений:", values_set)
 print("Объединение множеств:", union_set)
